f8a8f94b022a5331331e9543c1d9202e4fed7692
Cihangir Bozdogan <cihangirrbozzd@gmail.com>
2019-04-05 13:26:04 -0400

    posted advert page coded

---
+++ pages/components/user/user-adverts/user-adverts.js
@@ -0,0 +1,53 @@
+import { Fragment } from "react";
+import Router from "next/router";
+import Link from "next/link";
+const trashImg = "/static/images/icons/bin.svg";
+const editImg = "/static/images/icons/edit.svg";
+
+export default ({ posts }) => (
+  <Fragment>
+    <section className="posted mobile-desktop-frame">
+      <h1 className="section-main-title">Posted Adverts</h1>
+      {posts.map(({ url, title }) => (
+        <div className="console__profile-frame-property">
+          <Link href={`/property/${url}`}>
+            <a className="console__profile-frame-property-content">
+              <h4>{title.slice(0, 63)}...</h4>
+            </a>
+          </Link>
+          <div className="console__profile-frame-property-controls">
+            <Link href={`/user/advert/edit/${url}`}>
+              <a className="console__profile-frame-property-controls-edit">
+                <img
+                  src={editImg}
+                  title="Edit this advert"
+                  alt="Edit this advert"
+                />
+                <span>Edit</span>
+              </a>
+            </Link>
+            <Link href={`/api/user/advert/remove/${url}`}>
+              <a className="console__profile-frame-property-controls-remove">
+                <img
+                  src={trashImg}
+                  title="Remove this advert"
+                  alt="Remove this advert"
+                />
+                <span>Remove</span>
+              </a>
+            </Link>
+          </div>
+        </div>
+      ))}
+    </section>
+    <div className="go-to-previous-page mobile-desktop-frame">
+      <button
+        onClick={() => {
+          Router.back();
+        }}
+      >
+        Previous page
+      </button>
+    </div>
+  </Fragment>
+);
